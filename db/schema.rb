b32878c036a3724c12551f22e856948932b17e1e
Ivan <githubh@gmail.com>
2016-09-14 21:43:01 +0300

    Add column play_id to Reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160914183446) do
+ActiveRecord::Schema.define(version: 20160914184130) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20160914183446) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.integer  "play_id"
   end
 
   create_table "users", force: :cascade do |t|
